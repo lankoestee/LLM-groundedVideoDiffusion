a0582ac7ea8238dbdf1fbb20f8b92d6ffa867d0a
lankoestee <wpointpoint@outlook.com>
2024-05-14 10:36:23 +0800

    modify image channels

--- generation/lvd_gligen.py
+++ generation/lvd_gligen.py
@@ -121,6 +121,7 @@ def run(
     
     image = Image.open("images/boy.png")
     image = image.resize((512, 512))
+    image = image.convert('RGB')
     image_latents = encode(pipe, image, generator)
     print("----------")
     print("image_latents.shape = ", image_latents.shape)
