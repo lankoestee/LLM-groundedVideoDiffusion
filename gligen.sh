c7af9ddd5b5743a738ed5d5b56774fe6bc39990f
lankoestee <wpointpoint@outlook.com>
2024-05-14 09:00:54 +0800

    add sh

---
+++ gligen.sh
@@ -0,0 +1,9 @@
+python generate.py \
+--model gpt-4-1106-preview \
+--run-model lvd-gligen_zeroscope \
+--prompt-type demo \
+--save-suffix lvd_gligen \
+--template_version v0.1 \
+--seed_offset 0 \
+--num_frames 24 \
+--gligen_scheduled_sampling_beta 0.4
