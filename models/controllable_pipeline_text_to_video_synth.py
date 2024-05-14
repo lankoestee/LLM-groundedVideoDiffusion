1b6d3b15380e7e45db700450f04fc292194f4634
lankoestee <wpointpoint@outlook.com>
2024-05-14 09:07:42 +0800

    die test

--- models/controllable_pipeline_text_to_video_synth.py
+++ models/controllable_pipeline_text_to_video_synth.py
@@ -666,6 +666,7 @@ class TextToVideoSDPipeline(
         print("----------")
         print("TextToVideoSDPipeline is called.")
         print("----------")
+        return None
         # 0. Default height and width to unet
         height = height or self.unet.config.sample_size * self.vae_scale_factor
         width = width or self.unet.config.sample_size * self.vae_scale_factor
